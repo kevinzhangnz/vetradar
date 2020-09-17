0d5861625ecb37ed75cf346102ac5d202079da34
kevinzhangnz <51142004+kevinzhangnz@users.noreply.github.com>
2020-09-17 23:21:12 +1200

    setup swagger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { cartRouter } from './routes';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './swagger.json';
 
 // default port to listen
 const port = 3000;
@@ -9,6 +11,9 @@ app.use(express.json());
 
 // Configure routes
 app.use('/api/cart', cartRouter);
+
+// Configure swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // start the Express server
 app.listen(port, () => {
