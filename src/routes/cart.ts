1f9357d68b0d3b3ef5707c3f63fc4e9d0698fa02
kevinzhangnz <51142004+kevinzhangnz@users.noreply.github.com>
2020-09-17 22:24:45 +1200

    add POST item

--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -1,5 +1,6 @@
+import * as store from '../store';
 import express, { Request, Response } from 'express';
-import { Cart } from '../models';
+import { Cart, CartItem } from '../models';
 
 export const cartRouter = express.Router();
 
@@ -10,6 +11,7 @@ const cart: Cart = {
   totalPrice: 0,
   items: [],
 };
+const catalog: CartItem[] = store.Store;
 
 // GET cart
 cartRouter.get('/', (req: Request, res: Response) => {
@@ -17,5 +19,33 @@ cartRouter.get('/', (req: Request, res: Response) => {
     res.status(200).send(cart);
   } catch (e) {
     res.status(404).send(e.message);
+  }
+});
+
+// POST item
+cartRouter.post('/', (req: Request, res: Response) => {
+  try {
+    const id = req.body.id;
+    const qty = req.body.qty;
+    const index = cart.items.findIndex((obj) => obj.id === id);
+
+    index > -1
+      ? (() => {
+          cart.items[index].qty += qty;
+        })()
+      : (() => {
+          const matched: CartItem = catalog.filter((obj) => obj.id === id)[0];
+          const product: CartItem = {
+            id,
+            name: matched.name,
+            price: matched.price,
+            qty,
+          };
+          cart.items.push(product);
+        })();
+
+    res.status(201).send(cart);
+  } catch (e) {
+    res.status(400).send(e.message);
   }
 });
